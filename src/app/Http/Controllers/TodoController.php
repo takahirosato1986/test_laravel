68963f8408415e878280a926d3c02f983200415e
takahirosato1986 <acco.to.exc.i.opi@gmail.com>
2026-01-24 01:52:47 +0900

    バリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 use App\Todo;
 
 class TodoController extends Controller
@@ -26,7 +26,7 @@ class TodoController extends Controller
     return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
     $inputs = $request->all();
     $this->todo->fill($inputs);
@@ -46,12 +46,20 @@ class TodoController extends Controller
     return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
     $inputs = $request->all();
     $todo = $this->todo->find($id);
     $todo->fill($inputs)->save();
     return redirect()->route('todo.show', $todo->id);
+    }
+
+    public function messages()
+    {
+    return [
+        'content.required' => 'ToDoが入力されていません。',
+        'content.max' => 'ToDoは :max 文字以内で入力してください。',
+    ];
     }
 
 }
